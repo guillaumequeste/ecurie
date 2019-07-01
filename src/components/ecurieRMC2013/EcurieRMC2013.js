eb099ceb9a84ccc1fa5dc314aa3f51d659441e0b
Guillaume Queste <guillaume.queste@laposte.net>
2019-07-01 12:05:58 +0200

    page2013

--- src/components/ecurieRMC2013/EcurieRMC2013.js
+++ src/components/ecurieRMC2013/EcurieRMC2013.js
@@ -1,50 +1,23 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../Header'
+import './EcurieRMC2013.css'
 
 class EcurieRMC2013 extends Component {
   
     render () {
         return (
-            <div>
+            <div className="body2013">
                 <Header />
-                <h1>Ecurie RMC 2013</h1>
-                <Link to="/galopeurs2013">Galopeurs</Link>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
-                <p>fv</p>
+                <div className="div2013">
+                    <h1 className="titre2013">Ecurie RMC 2013</h1>
+                    <div className="card" style={{width:'400px'}}>
+                        <img className="card-img-top" src={require("../../img/2013/galopeurs2013/paint.jpg")} alt="paint" style={{width:'400px'}}/>
+                        <div className="card-body" style={{backgroundColor:'grey',textAlign:'center'}}>
+                            <Link to="/galopeurs2013" className="btn btn-primary">Galopeurs 2013</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
