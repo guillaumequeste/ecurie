2335eac332f3da76d93edca4d5ab56a3ef9b02c1
Guillaume Queste <guillaume.queste@laposte.net>
2019-08-14 10:31:40 +0200

    14 août 2019

--- src/components/Bilan.js
+++ src/components/Bilan.js
@@ -12,7 +12,7 @@ class Bilan extends Component {
                     <p><strong>RMC 2013</strong> : 105 courses, 7 victoires, 36 places</p>
                     <p><strong>RMC 2014</strong> : 191 courses, 20 victoires, 83 places</p>
                     <p><strong>RMC 2015</strong> : 78 courses, 9 victoires, 36 places</p>
-                    <p><strong>Club RMC</strong> : 228 courses, 13 victoires, 112 places</p>
+                    <p><strong>Club RMC</strong> : 229 courses, 13 victoires, 112 places</p>
                     <p><strong>XXL</strong> : 7 courses, 4 places</p>
                 </div>
             </div>
