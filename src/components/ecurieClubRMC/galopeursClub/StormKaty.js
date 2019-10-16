f368db3d3550b19d4eddfe3974b5937fb616bbb6
Guillaume Queste <guillaume.queste@laposte.net>
2019-10-16 10:49:36 +0200

    16 octobre 2019

--- src/components/ecurieClubRMC/galopeursClub/StormKaty.js
+++ src/components/ecurieClubRMC/galopeursClub/StormKaty.js
@@ -6,12 +6,20 @@ class StormKaty extends Component {
   
     render () {
         return (
-            <div className="bodyChevalStormKaty">
+            <div className="bodyCheval">
                 <Header />
                 <h1 className="nomCheval">Storm Katy</h1>
                 <img className="imgDetail" src={require("../../../img/ClubRMC/galopeursClub/storm_katy/storm_katy1.jpg")} alt="storm_katy"/>
                 <p>1 course</p>
                 <p>1 place</p>
+                <div className="row">
+                    <div className="col-sm-3">
+                        <img className="imgDetail2" src={require("../../../img/ClubRMC/galopeursClub/storm_katy/storm_katy2.jpg")} alt="storm_katy2"/>
+                    </div>
+                    <div className="col-sm-3">
+                        <img className="imgDetail2" src={require("../../../img/ClubRMC/galopeursClub/storm_katy/storm_katy3.jpg")} alt="storm_katy3"/>
+                    </div>
+                </div>
             </div>
         )
     }
