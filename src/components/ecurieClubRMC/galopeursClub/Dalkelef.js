6d3e2d41b447da94936054fc625269806183fe8b
Guillaume Queste <guillaume.queste@laposte.net>
2019-12-04 17:31:46 +0100

    4 décembre 2019

--- src/components/ecurieClubRMC/galopeursClub/Dalkelef.js
+++ src/components/ecurieClubRMC/galopeursClub/Dalkelef.js
@@ -10,8 +10,8 @@ class Dalkelef extends Component {
                 <Header />
                 <h1 className="nomCheval">Dalkelef</h1>
                 <img className="imgDetail" src={require("../../../img/ClubRMC/galopeursClub/dalkelef/dalkelef1.jpg")} alt="dalkelef"/>
-                <p>7 courses</p>
-                <p>2 places</p>
+                <p>8 courses</p>
+                <p>3 places</p>
             </div>
         )
     }
