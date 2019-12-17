a0538389ddf311f5868c53771a3b0446a639ae40
Guillaume Queste <guillaume.queste@laposte.net>
2019-12-17 14:31:27 +0100

    17 décembre 2019

--- src/components/ecurieClubRMC/galopeursClub/DivaDuDancing.js
+++ src/components/ecurieClubRMC/galopeursClub/DivaDuDancing.js
@@ -10,9 +10,9 @@ class DivaDuDancing extends Component {
                 <Header />
                 <h1 className="nomCheval">Diva du Dancing</h1>
                 <img className="imgDetail" src={require("../../../img/ClubRMC/galopeursClub/diva/diva1.jpg")} alt="diva_du_dancing"/>
-                <p>7 courses</p>
+                <p>8 courses</p>
                 <p>1 victoire à Salon de Provence</p>
-                <p>6 places</p>
+                <p>7 places</p>
             </div>
         )
     }
