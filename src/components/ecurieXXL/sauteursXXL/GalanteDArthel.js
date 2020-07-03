6a9b862becfb9ed03b4a36439eccee953f95b948
Guillaume Queste <guillaume.queste@laposte.net>
2020-07-03 10:10:00 +0200

    3 juillet 2020

--- src/components/ecurieXXL/sauteursXXL/GalanteDArthel.js
+++ src/components/ecurieXXL/sauteursXXL/GalanteDArthel.js
@@ -10,8 +10,8 @@ class GalanteDArthel extends Component {
                 <Header />
                 <h1 className="nomCheval">Galante d'Arthel</h1>
                 <img className="imgDetail" src={require("../../../img/XXL/sauteursXXL/galante/galante1.jpg")} alt="galante_d_arthel"/>
-                <p>5 courses</p>
-                <p>4 places</p>
+                <p>6 courses</p>
+                <p>5 places</p>
                 <div className="row">
                     <div className="col-sm-3">
                         <img className="imgDetail2" src={require("../../../img/XXL/sauteursXXL/galante/galante2.jpg")} alt="galante2"/>
