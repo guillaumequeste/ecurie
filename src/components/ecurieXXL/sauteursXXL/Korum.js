a9541199cbf7a3faebd6353eb3b535e4780487bb
Guillaume Queste <guillaume.queste@laposte.net>
2020-07-10 09:33:37 +0200

    10 juillet 2020

--- src/components/ecurieXXL/sauteursXXL/Korum.js
+++ src/components/ecurieXXL/sauteursXXL/Korum.js
@@ -10,9 +10,9 @@ class Korum extends Component {
                 <Header />
                 <h1 className="nomCheval">Korum</h1>
                 <img className="imgDetail" src={require("../../../img/XXL/sauteursXXL/korum/korum1.jpg")} alt="korum"/>
-                <p>5 courses</p>
+                <p>6 courses</p>
                 <p>1 victoire à Lyon Parilly</p>
-                <p>3 places</p>
+                <p>4 places</p>
                 <div className="row">
                     <div className="col-sm-3">
                         <img className="imgDetail2" src={require("../../../img/XXL/sauteursXXL/korum/korum2.jpg")} alt="korum2"/>
