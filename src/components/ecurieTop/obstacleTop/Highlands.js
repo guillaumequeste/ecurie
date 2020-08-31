5e03cad2a96f2b6c1ee25c8d8c80cc5d7a1abc90
Guillaume Queste <guillaume.queste@laposte.net>
2020-08-31 11:26:43 +0200

    31 août 2020

--- src/components/ecurieTop/obstacleTop/Highlands.js
+++ src/components/ecurieTop/obstacleTop/Highlands.js
@@ -6,11 +6,17 @@ class Highlands extends Component {
   
     render () {
         return (
-            <div className="bodyChevalHighlands">
+            <div className="bodyCheval">
                 <Header />
                 <h1 className="nomCheval">Highlands d'Allier</h1>
                 <img className="imgDetail" src={require("../../../img/Top/obstacleTop/highlands/highlands01.jpg")} alt="highlands01"/>
                 <p>1 course</p>
+                <p>1 place</p>
+                <div className="row">
+                    <div className="col-sm-3">
+                        <img className="imgDetail2" src={require("../../../img/Top/obstacleTop/highlands/highlands02.jpg")} alt="highlands02"/>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/ecurieXXL/sauteursXXL/EclairEtincelle.js
+++ src/components/ecurieXXL/sauteursXXL/EclairEtincelle.js
@@ -10,8 +10,9 @@ class EclairEtincelle extends Component {
                 <Header />
                 <h1 className="nomCheval">Eclair Etincelle</h1>
                 <img className="imgDetail" src={require("../../../img/XXL/sauteursXXL/eclair/eclair1.jpg")} alt="eclair_etincelle"/>
-                <p>5 courses</p>
+                <p>6 courses</p>
                 <p>1 victoire à Sablé-sur-Sarthe</p>
+                <p>1 place</p>
                 <div className="row">
                     <div className="col-sm-3">
                         <img className="imgDetail2" src={require("../../../img/XXL/sauteursXXL/eclair/eclair2.jpg")} alt="eclair2"/>
