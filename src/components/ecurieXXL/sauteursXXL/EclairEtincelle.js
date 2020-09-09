4721a5da0e8552380b40f46690c50f901f7f5289
Guillaume Queste <guillaume.queste@laposte.net>
2020-09-09 19:10:35 +0200

    9 septembre 2020

--- src/components/ecurieXXL/sauteursXXL/EclairEtincelle.js
+++ src/components/ecurieXXL/sauteursXXL/EclairEtincelle.js
@@ -10,9 +10,9 @@ class EclairEtincelle extends Component {
                 <Header />
                 <h1 className="nomCheval">Eclair Etincelle</h1>
                 <img className="imgDetail" src={require("../../../img/XXL/sauteursXXL/eclair/eclair1.jpg")} alt="eclair_etincelle"/>
-                <p>6 courses</p>
+                <p>7 courses</p>
                 <p>1 victoire à Sablé-sur-Sarthe</p>
-                <p>1 place</p>
+                <p>2 places</p>
                 <div className="row">
                     <div className="col-sm-3">
                         <img className="imgDetail2" src={require("../../../img/XXL/sauteursXXL/eclair/eclair2.jpg")} alt="eclair2"/>
