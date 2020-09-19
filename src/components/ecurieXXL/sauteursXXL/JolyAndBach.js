feaf2df05edd787b239a38d6924738d9db93964d
Guillaume Queste <guillaume.queste@laposte.net>
2020-09-19 13:20:15 +0200

    19 septembre 2020

--- src/components/ecurieXXL/sauteursXXL/JolyAndBach.js
+++ src/components/ecurieXXL/sauteursXXL/JolyAndBach.js
@@ -10,7 +10,8 @@ class JolyAndBach extends Component {
                 <Header />
                 <h1 className="nomCheval">Joly And Bach</h1>
                 <img className="imgDetailJolyAndBach" src={require("../../../img/XXL/sauteursXXL/joly/joly1.jpg")} alt="joly_and_bach"/>
-                <p>2 courses</p>
+                <p>3 courses</p>
+                <p>1 place</p>
             </div>
         )
     }
