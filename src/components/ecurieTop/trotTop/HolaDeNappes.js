298d08281fd22c3053606e7c97e6bf9f29ae29c0
Guillaume Queste <guillaume.queste@laposte.net>
2020-12-22 10:25:08 +0100

    22 decembre 2020

--- src/components/ecurieTop/trotTop/HolaDeNappes.js
+++ src/components/ecurieTop/trotTop/HolaDeNappes.js
@@ -10,8 +10,8 @@ class HolaDeNappes extends Component {
                 <Header />
                 <h1 className="nomCheval">Hola de Nappes</h1>
                 <img className="imgDetail" src={require("../../../img/Top/trotTop/hola/hola01.jpg")} alt="hola01"/>
-                <p>1 course</p>
-                <p>1 place</p>
+                <p>2 courses</p>
+                <p>2 places</p>
                 <div className="row">
                     
                 </div>
